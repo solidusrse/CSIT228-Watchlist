faa1a299f50e900ce9bd15cb8494012b30adc104
Sheer Jay A. Piodos <sheerjay.piodos@cit.edu>
2023-05-07 21:37:35 +0800

    Merge origin/master
    Conflicts:
    	src/Connect.java

---
+++ src/Connect.java
@@ -0,0 +1,198 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * 
+ */
+public class Connect {
+    
+    Connection conn=null;
+    public Connect() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/dbmovieseries","root","d3rp1ng1");
+            JOptionPane.showMessageDialog(null, "Connected");
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    Connect(String jdbcmysqllocalhost3307dbmovieseries, String root, String d3rp1ng1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    public boolean addMovieSeries(MovieSeries movieseries) {
+        Statement stmt;
+        String sql=null;
+        ResultSet rs=null;
+        try {
+            stmt = conn.createStatement();
+            sql="select * from tblmovieseries where title='"+movieseries.getTitle()+"'";
+            rs = stmt.executeQuery(sql);
+            if(rs.next()==false){
+                sql="insert into tblmovieseries values('"+movieseries.getTitle()+"',"+movieseries.getEpisodes()+",'"+movieseries.getType()+"' ,"+movieseries.getWatchStatus()+", '"+movieseries.getGenre()+"', '"+movieseries.getSynopsis()+"')";
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Movie successfully added!");
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+    
+    public boolean favorite(MovieSeries favorite) {
+        Statement stmt;
+        String sql=null;
+        ResultSet rs=null;
+        try {
+            stmt = conn.createStatement();
+            sql="select * from tblfavorites where title='"+favorite.getTitle()+"'";
+            rs = stmt.executeQuery(sql);
+            if(rs.next()==false){
+                sql="insert into tblfavorites values('"+favorite.getTitle()+"','"+favorite.getType()+"',"+favorite.getEpisodes()+" ,'"+favorite.getType()+"')";
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Successfully Added!");
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+    
+    
+    
+    public ArrayList<MovieSeries> searchMovieSeries(String str){
+        ArrayList<MovieSeries> mvs = new ArrayList<>();
+        Statement stmt;
+        String sql=null;
+        ResultSet rs=null;
+        try {
+            stmt = conn.createStatement();
+            sql = "select * from tblmovieseries where title like '%"+str+"%' or genre like '%"+str+"%'";
+            rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                MovieSeries movieseries = new MovieSeries(rs.getString(1), rs.getInt(2), rs.getString(3), rs.getString(5), rs.getString(6));
+                mvs.add(movieseries);
+            }
+            return mvs;
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+        return null;
+    }
+    
+    public boolean updateWatchStatus(MovieSeries ms){
+        Statement stmt;
+        String sql;
+        try {
+            stmt = conn.createStatement();
+            sql = "UPDATE tblmovieseries SET watchstatus = "+ms.getWatchStatus()+" WHERE title = '"+ms.getTitle()+"'";
+            stmt.executeUpdate(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
+    }    
+    
+    public int login(String username, String password){
+        Statement stmt;
+        String sql;
+        ResultSet rs;
+        try {
+            stmt=conn.createStatement();
+            sql ="select * from tbluser where username='"+username+"' and password='"+password+"'";
+            rs = stmt.executeQuery(sql);
+            if (rs.next()==true)
+                return 1;
+            else
+                return 0;
+        } catch (SQLException ex) {
+            // Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
+    
+    public boolean register(User user){
+        Statement stmt;
+        String sql;
+        ResultSet rs;
+        try {
+            stmt = conn.createStatement();
+            sql="select * from tbluser where username='"+user.getUsername()+"'";
+            rs =stmt.executeQuery(sql);
+            if(rs.next()==false){
+                sql="insert into tbluser values('"+user.getUsername()+"','"+user.getPassword()+"','"+user.getFirstname()+"','"+user.getLastname()+"')";
+                stmt.executeUpdate(sql);
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+    
+    public void AddToWatchList(String name, String genre, int duration){
+        Statement stmt;
+        String sql =null;
+        ResultSet rs=null;
+        
+        try {
+            stmt = conn.createStatement();
+            sql ="select * from tblwatchlist where title='"+name+"'";
+            rs = stmt.executeQuery(sql);
+            if(rs.next() == false){
+                sql="insert into tblwatchlist(title,genre,duration) values('"+name+"','"+genre+"',duration)";
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null,"New record saved");
+            }else{
+                JOptionPane.showMessageDialog(null,"Movie already existing.");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void RemoveFromWatchList(String name){
+        Statement stmt;
+        String sql =null;
+        ResultSet rs=null;
+        
+        try {
+            stmt = conn.createStatement();
+            sql ="delete from tblwatchlist where title='"+name+"'";
+            stmt.executeUpdate(sql);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/MovieSeries.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author josia
+ */
+public class MovieSeries {
+    private String title;
+    private int episodes;
+    private String type;
+    private int watchStatus;
+    private String genre;
+    private String synopsis;
+
+    public MovieSeries(String title, int episodes, String type, String genre) {
+        this.title = title;
+        this.episodes = episodes;
+        this.type = type;
+        this.watchStatus = 0;
+        this.genre = genre;
+        this.synopsis = " ";
+    }
+    
+    public MovieSeries(String title, int episodes, String type, String genre, String synopsis) {
+        this.title = title;
+        this.episodes = episodes;
+        this.type = type;
+        this.watchStatus = 0;
+        this.genre = genre;
+        this.synopsis = synopsis;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public int getWatchStatus() {
+        return watchStatus;
+    }
+
+    public void setWatchStatus(int watchStatus) {
+        this.watchStatus = watchStatus;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+}
